2ce3b990deb62a89ee78372c0cfb07a79c476074
platypus012 <124108629+platypus012@users.noreply.github.com>
2023-02-02 16:48:09 +0900

    Update Q4.py

--- Week01/Python/Chapter 1/Q4.py
+++ Week01/Python/Chapter 1/Q4.py
@@ -4,9 +4,12 @@
 
 data = input('숫자로 이루어진 문자열을 입력하세요. ')
 result = 1
+
 for i in range(len(data)):
-    if data[i] == '0':
+    if data[i] == '0' :
         continue
+    elif data[i] == '1':
+        result += int(data[i])
     else:
         result *= int(data[i])
 
